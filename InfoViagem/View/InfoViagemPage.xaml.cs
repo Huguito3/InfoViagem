b84edc56e5430acfdd58a5f606c779ac5058e370
diogobr07 <diogobruno07@gmail.com>
2017-01-06 21:34:24 -0200

    adicionada classe para data binding da primeira tela

--- InfoViagem/View/InfoViagemPage.xaml.cs
+++ InfoViagem/View/InfoViagemPage.xaml.cs
@@ -4,123 +4,134 @@ using System.Diagnostics;
 using System.Net;
 using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
+using InfoViagem.ViewModel;
 
 namespace InfoViagem
 {
-	public partial class InfoViagemPage : ContentPage
-	{
-		Cidades ciudadOrigem;
-		Cidades ciudadDestino;
-		public string Origem{get; set;}
-		public string Destino { get; set;}
-		private string origem;
-		public InfoViagemPage()
-		{
-			InitializeComponent();
-			BindingContext = this;
+    public partial class InfoViagemPage : ContentPage
+    {
+        Cidades ciudadOrigem;
+        Cidades ciudadDestino;
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+        public InfoViagemViewModel infonViagem;
+        public InfoViagemPage()
+        {
+            InitializeComponent();
+            infonViagem = new InfoViagemViewModel();
+            //BindingContext = this;
+            BindingContext = infonViagem;
 
-		}
+        }
 
-		async void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
-		{
+        async void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
 
-			string teste = e.NewTextValue;
-			if (teste.Length > 4)
-			{
-				//string[] arstri;
-				List<string> arstri = new List<string>();
-				//var action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null,arstri);
+            string teste = e.NewTextValue;
+            if (teste.Length > 4)
+            {
+                //string[] arstri;
+                List<string> arstri = new List<string>();
+                //var action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null,arstri);
 
-				RestService service = new RestService();
-				ResultadosLista listaCidades = await service.getCidadeAsync(teste);
-				foreach (Cidades cidade in listaCidades.Results)
-				{
-					arstri.Add(cidade.name + "-" + cidade.c);
-				}
-				var action = await DisplayActionSheet("Escolha sua cidade", "Cancel", null, arstri.ToArray());
-				string[] busqueda = action.Split('-');
-				foreach (Cidades cidade in listaCidades.Results)
-				{
-					if ((cidade.name.Contains(busqueda[0])) && (cidade.c.Contains(busqueda[1])))
-					{
-						ciudadOrigem = cidade;
-						Origem = cidade.name;
+                RestService service = new RestService();
+                ResultadosLista listaCidades = await service.getCidadeAsync(teste);
+                if (listaCidades.Results.Count > 1)
+                {
+                    foreach (Cidades cidade in listaCidades.Results)
+                    {
+                        arstri.Add(cidade.name + "-" + cidade.c);
+                    }
+                    var action = await DisplayActionSheet("Escolha sua cidade", "Cancel", null, arstri.ToArray());
+                    string[] busqueda = action.Split('-');
+                    foreach (Cidades cidade in listaCidades.Results)
+                    {
+                        if ((cidade.name.Contains(busqueda[0])) && (cidade.c.Contains(busqueda[1])))
+                        {
+                            ciudadOrigem = cidade;
+                            infonViagem.Origem = cidade.name;
 
-					}
-				}
-			}
+                        }
+                    }
 
-
-		}
-
-		async void Destino_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
-		{
-
-			string teste = e.NewTextValue;
-			if (teste.Length > 4)
-			{
-				//string[] arstri;
-				List<string> arstri = new List<string>();
-				//var action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null,arstri);
-
-				RestService service = new RestService();
-				ResultadosLista listaCidades = await service.getCidadeAsync(teste);
-				foreach (Cidades cidade in listaCidades.Results)
-				{
-					arstri.Add(cidade.name + "-" + cidade.c);
-				}
-				var action = await DisplayActionSheet("Escolha sua cidade", "Cancel", null, arstri.ToArray());
-				string[] busqueda = action.Split('-');
-				foreach (Cidades cidade in listaCidades.Results)
-				{
-					if ((cidade.name.Contains(busqueda[0])) && (cidade.c.Contains(busqueda[1])))
-					{
-						ciudadDestino = cidade;
-
-					}
-				}
-			}
+                }
+            }
 
 
-		}
+        }
 
-		async void Handle_Clicked(object sender, System.EventArgs e)
-		{
-			RestService service = new RestService();
-			var resultado = await service.getWeatherAsync(ciudadDestino.lat, ciudadDestino.lon);
+        async void Destino_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
 
-			var resul = await service.convertToCurrencyAsync(verificarCidade(ciudadDestino.c), 1, verificarCidade(ciudadOrigem.c));
+            string teste = e.NewTextValue;
+            if (teste.Length > 4)
+            {
+                //string[] arstri;
+                List<string> arstri = new List<string>();
+                //var action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null,arstri);
 
-			//await DisplayAlert("Teste", resultado.ToString(), "Cancel");
-			await Navigation.PushAsync(new Detalhes(ciudadOrigem,ciudadDestino,resul.to_amount,resultado));
+                RestService service = new RestService();
+                ResultadosLista listaCidades = await service.getCidadeAsync(teste);
+                if (listaCidades.Results.Count > 1)
+                {
+                    foreach (Cidades cidade in listaCidades.Results)
+                    {
+                        arstri.Add(cidade.name + "-" + cidade.c);
+                    }
+                    var action = await DisplayActionSheet("Escolha sua cidade", "Cancel", null, arstri.ToArray());
+                    string[] busqueda = action.Split('-');
+                    foreach (Cidades cidade in listaCidades.Results)
+                    {
+                        if ((cidade.name.Contains(busqueda[0])) && (cidade.c.Contains(busqueda[1])))
+                        {
+                            ciudadDestino = cidade;
+                            infonViagem.Destino = cidade.name;
 
-		}
-
-		private string verificarCidade(string pais)
-		{
-			string moeda="USD";
-			if (pais == "BR")
-			{
-				moeda = "BRL";
-			}
-			else if (pais == "US")
-			{
-				moeda = "USD";
-			}
-			else if (pais == "FR" || pais == "ES" || pais == "IT"|| pais == "PT")
-			{
-				moeda = "EUR";
-			}
-			else if (pais == "GB")
-			{
-				moeda = "GBP";
-			}
-
-
-			return moeda;
-
-		}
+                        }
+                    }
+                }
+            }
 
 
-	}
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            RestService service = new RestService();
+            var resultado = await service.getWeatherAsync(ciudadDestino.lat, ciudadDestino.lon);
+
+            var resul = await service.convertToCurrencyAsync(verificarCidade(ciudadDestino.c), 1, verificarCidade(ciudadOrigem.c));
+
+            //await DisplayAlert("Teste", resultado.ToString(), "Cancel");
+            await Navigation.PushAsync(new Detalhes(ciudadOrigem, ciudadDestino, resul.to_amount, resultado));
+
+        }
+
+        private string verificarCidade(string pais)
+        {
+            string moeda = "USD";
+            if (pais == "BR")
+            {
+                moeda = "BRL";
+            }
+            else if (pais == "US")
+            {
+                moeda = "USD";
+            }
+            else if (pais == "FR" || pais == "ES" || pais == "IT" || pais == "PT")
+            {
+                moeda = "EUR";
+            }
+            else if (pais == "GB")
+            {
+                moeda = "GBP";
+            }
+
+
+            return moeda;
+
+        }
+
+
+    }
 }
---
+++ InfoViagem/ViewModel/InfoViagemViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoViagem.ViewModel
+{
+    public class InfoViagemViewModel : INotifyPropertyChanged
+    {
+        string origem, destino;
+
+        public string Origem
+        {
+            get
+            {
+                return origem;
+            }
+            set
+            {
+                if (origem != value)
+                {
+                    origem = value;
+                    OnPropertyChanged("Origem");
+                }
+            }
+        }
+        public string Destino
+        {
+            get
+            {
+                return destino;
+            }
+            set
+            {
+                if (destino != value)
+                {
+                    destino = value;
+                    OnPropertyChanged("Destino");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
